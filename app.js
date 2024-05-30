b0f2db198878b1ea08b933b1435e82e788d35e75
Abdumajodov Laziz <153080975+Laziz0627@users.noreply.github.com>
2024-05-30 15:36:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+// // let son = Number(prompt("ixtiyoriy son qoying: "))
+
+// // let resultl = "";
+
+// // if (son % 2 == 0) {
+// //     resultl = "juf son";
+// //     console.log(resultl)
+// // } 
+// // else if(son % 2 == 1) {
+// //     resultl = "toq son";
+// //     console.log(resultl)
+// // }
+
+
+// // let bal = prompt("toplagan balni kiriting") 
+
+// // if (bal <= 80) {
+// //     console.log('yiqildingiz')
+// // }
+// // else if (bal <= 85) {
+// //     console.log('sizning baxoyingiz 4 bal')
+// // }
+// // else if (bal <= 96) {
+// //     console.log('sizning baxoyingiz 5 bal')
+// // }
+// // else if (bal <= 100) {
+// //     console.log('siz bujutsiz')
+// // }
+
+// // let yosh = prompt('siz necha yoshdasiz')
+
+// // if(yosh > 16) {
+// //     console.log('kirish mumkin');
+// // } else console.log('kirish taqiqlandi');
+
+
+//    let input = document.elementbyid('inputBox');
+//    let buttons = document.selectorsall('button')
+
+//    let string = "";
+//    let arr = Array.from(buttons);
+//    arr.foreach(button => {
+//     button.addEventListener('click', (e) =>{
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'AC'){
+//             string = "";
+//             input.value = string
+//         }
+//         else if(e.target.innerHTML == 'DE'){
+//             string = string.substring(0, string.lenght-1);
+//             input.value = string;
+//         }
+//         else{
+//             string += e.target.innerHTML;
+//             input.value = string;
+//         }
+//     })
+//    })
+function num1(num1) {
+    if (num2 + 4 === 3) {
+        console.log(7);
+    } else if (num3 - 4 === 3) {
+        console.log(1);
+    }
+}
+num1()
+
+
+// var num1 =5,
+// num2 = 4,
+// amal = '*';
+
+// if (amal === '+') {
+//     console.log(num1+num2);
+// } else if (amal ==='-') {
+//     console.log(num1-num2);
+// } else if (amal ==='*') {
+//     console.log(num1*num2);
+// } else if (amal ==='/') {
+//     console.log(num1/num2);
+// }
+
+function Even(number) {
+    if (number % 2 === 0) {
+        console.log('juft son');
+    } else {
+        console.log('tog son');
+
+    }
+}
+Even(7)
+Even(4)
+Even(7)
+Even(7)
